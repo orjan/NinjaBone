7aab21d6d43c9a2ec35edbdc08a643efe45d51d9
Örjan Sjöholm <orjan.sjoholm@tretton37.com>
2011-08-19 16:46:35 +0200

    Added a simple ninja view

---
+++ NinjaBone/Controllers/NinjaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NinjaBone.Controllers
+{
+    public class NinjaController : Controller
+    {
+        public ActionResult View(int id)
+        {
+            return View();
+        }
+    }
+}
