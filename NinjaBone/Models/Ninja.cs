e84a2dce306dbe4090c8171ac7a4017e0e9c0912
Örjan Sjöholm <orjansjoholm@gmail.com>
2011-09-10 10:03:48 +0200

    Added all ninjas to the same page. This makes it possible to cache a single page and view all ninjas without js

--- NinjaBone/Models/Ninja.cs
+++ NinjaBone/Models/Ninja.cs
@@ -2,9 +2,10 @@
 {
     public class Ninja
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
         public string Phone { get; set; }
-
-        public int Id { get; set; }
+        public string Email { get; set; }
     }
 }
