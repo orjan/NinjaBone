4b873e805dd561d157344bd0e9161abadd13130a
Örjan Sjöholm <orjansjoholm@gmail.com>
2011-10-08 11:05:48 +0200

    Updated jquery mobile Added a simple page for mad skills

---
+++ NinjaBone.Web/Controllers/SkillsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NinjaBone.Web.Controllers
+{
+    public class SkillsController : Controller
+    {
+        //
+        // GET: /Skills/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
